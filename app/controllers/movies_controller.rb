8248e1ce32d2b9dbb9995e65e6562add544960f8
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-11-08 05:06:11 +0000

    movies#create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,26 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    # Retrieve the user's inputs from params
+      # params hash looks like this
+      # {"query_title"=>"1", "query_year"=>"2", "query_duration"=>"3", "query_description"=>"4", "query_image"=>"5", "query_director_id"=>"6"}
+    m = Movie.new
+    m.title = params.fetch("query_title")
+    m.year = params.fetch("query_year")
+    m.duration = params.fetch("query_duration")
+    m.description = params.fetch("query_description")
+    m.image = params.fetch("query_image")
+    m.director_id = params.fetch("query_director_id")
+
+    m.save
+
+    redirect_to("/movies")
+    # Create a record in the movie table
+    # Populate each column with the user input
+    # Save
+    # Redirect the user back to the /movies URL
+  end
+
 end
